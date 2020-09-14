a83ea5b10cd6acf35e7a30a8cf9302f6cda6b1cd
serendatapy <arvaldivia@gmail.com>
2020-09-14 18:44:33 +0200

    Merging

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-=======
 import Timer from "./Timer";
->>>>>>> e8baaa191653461d9d84667f9c4607858a49e28e
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Timer />
->>>>>>> e8baaa191653461d9d84667f9c4607858a49e28e
     </div>
   );
 }
