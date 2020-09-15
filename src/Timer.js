0d26dacb073c0d1ef8c2601c460ec4da9e539dcd
Koz <kgrachev@gmail.com>
2020-09-15 01:23:09 +0100

    Implemented counting seconds with start/pause and reset buttons using setTimeout state hooks and effect hook.

--- src/Timer.js
+++ src/Timer.js
@@ -2,29 +2,41 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function Timer(props) {
-  const [seconds, setSeconds] = useState(0);
-  const [count, setCount] = useState(0);
+  const [seconds, setSeconds] = useState(0); // The value (state) does not actually actually change. The function is rerendered with a whole new variable that just happens to have the same name.
+  const [isRunning, setIsRunning] = useState(false); // HOOKS: .....
 
   useEffect(() => {
-
-  }, [seconds, ]);
+    if (isRunning) {
+      const id = window.setTimeout(() => {             // Allows us to call set the next value of seconds only once during this render
+        setSeconds(seconds => seconds + 1)
+      } // Passing a callback to setSeconds stops the change in seconds triggering the next render.
+        // It allows react to know what the value of seconds will be before actually assigning it.
+        , 1000                              // It should assign only after the interval has elapsed and not when this part of the code is rendered.
+      );
+      return () => window.clearTimeout(id); //cleanup function:....
+    }
+  }, [seconds, isRunning]); // Dependency array - rerender this entire function in order to change the value of seconds. But we don't want to do this because
+                            // Uses shallow equality - so if the value is the same, it will not rerender, even if the object is different.
 
   return (
     <div id="timerContainer">
       <div id="timeDisplay">
         seconds: {seconds}
-        <br/>
-        count: {count}
       </div>
 
       <div id="buttons">
-        <button id="startButton">Start</button>
-        <button id="pauseButton">Pause</button>
-        <button id="resetButton">Reset</button>
+        { isRunning ?
+          <button id="pauseButton" onClick={() => setIsRunning(!isRunning)}>Pause</button> :
+          <button id="playButton" onClick={() => setIsRunning(!isRunning)}>Play</button> // Good buttons only flip a boolean. They don't contain any complex logic
+        }
+        <button
+          id="resetButton"
+          disabled={seconds === 0 && !isRunning}
+          onClick={() =>{
+          setSeconds(0);
+          setIsRunning(false);
+          }}>Reset</button>
       </div>
-
-      <button onClick={() => setSeconds(seconds + 1)}>inc seconds</button>
-      <button onClick={() => setCount(count + 1)}>inc count</button>
     </div>
   )
 }
