0707c1b46ef97b8f89986405ea8c0b515dcccff4
Matt Hurst <matt.adam.hurst@gmail.com>
2020-09-16 15:54:53 +0100

    create input file and functionality

---
+++ src/Input.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+const Input = (props) => {
+    const [userInput, setUserInput] = useState(""); 
+
+    function handleChange(event) {
+        setUserInput(event.target.value)
+    }
+    console.log('userInput: ',userInput)
+    return (
+        <div>
+            <input 
+                onChange={handleChange}
+                placeholder="minutes"
+                type="number"
+                name="minutes"
+            />
+            <button
+                onClick={() => {
+                    // props.setWorkTime(userInput);
+                    setUserInput("");
+                }}
+            >Submit</button>
+        </div>
+    )
+
+}
+
+export default Input;
