834507bf05ab0f0237ae32ac0f02a3d62561ff12
Koz <kgrachev@gmail.com>
2020-09-24 20:53:05 +0000

    gh-pages

---
+++ precache-manifest.c16f43c2be64059d1a3aa90a50a2aab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5409184ef2f2996422e289d106a591",
+    "url": "/Tomatracker/index.html"
+  },
+  {
+    "revision": "7266e6f7fa385b50881b",
+    "url": "/Tomatracker/static/css/main.5544bcae.chunk.css"
+  },
+  {
+    "revision": "1a4531663a60351c1b2a",
+    "url": "/Tomatracker/static/js/2.1591fb37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tomatracker/static/js/2.1591fb37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7266e6f7fa385b50881b",
+    "url": "/Tomatracker/static/js/main.00391a44.chunk.js"
+  },
+  {
+    "revision": "2d03610730d3c64d60ea",
+    "url": "/Tomatracker/static/js/runtime-main.cab1e2a6.js"
+  }
+]);
